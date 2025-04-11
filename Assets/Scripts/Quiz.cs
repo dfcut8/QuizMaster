f34b32e5f45188aa451dd225fff3a3ada6bff861
dfcut8 <dfcut8@proton.me>
2025-04-11 10:56:01 -0400

    handle correct and incorrect answers

--- Assets/Scripts/Quiz.cs
+++ Assets/Scripts/Quiz.cs
@@ -1,20 +1,42 @@
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
+using Mono.Cecil;
 
 public class Quiz : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI questionText;
     [SerializeField] private QuestionSO question;
     [SerializeField] private GameObject[] answerButtons;
+    [SerializeField] private Sprite defaultAnswerSprite;
+    [SerializeField] private Sprite correctAnswerSprite;
+    private int correctAnswerIndex;
 
     void Start()
     {
         questionText.text = question.GetQuestion();
         var answers = question.GetAnswers();
-        for (int i = 0; i < 4; i++)
+        for (int i = 0; i < answerButtons.Length; i++)
         {
             var buttonText = answerButtons[i].GetComponentInChildren<TextMeshProUGUI>();
             buttonText.text = answers[i];
+        }
+    }
+
+    public void OnAnswerSelected(int index)
+    {
+        if (index == question.GetCorrectAnswerIndex())
+        {
+            questionText.text = "Correct!";
+            Image buttonImage = answerButtons[index].GetComponent<Image>();
+            buttonImage.sprite = correctAnswerSprite;
+        }
+        else
+        {
+            var correctAnswerText = question.GetAnswers()[correctAnswerIndex];
+            questionText.text = $"BAD BAD BAD! :(\nCorrect answer: {correctAnswerText}";
+            Image buttonImage = answerButtons[correctAnswerIndex].GetComponent<Image>();
+            buttonImage.sprite = correctAnswerSprite;
         }
     }
 }
